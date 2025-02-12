2d835fa56c358f419cee5117c43a0bbb74fa7463
satyacomputerguide <160415818+satyacomputerguide@users.noreply.github.com>
2025-02-12 17:57:33 +0530

    My Porfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    
+    if(name && email && message) {
+        alert("Your message has been sent!");
+        document.querySelector("form").reset();
+    } else {
+        alert("Please fill out all fields.");
+    }
+});
